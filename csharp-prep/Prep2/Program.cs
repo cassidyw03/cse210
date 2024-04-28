11e3a8514e36203fc0c3947956d5925af59fb527
cassidyw03 <whi21049@byui.edu>
2024-04-27 23:21:26 -0600

    Prep 1 && Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage?");
+        string percentage = Console.ReadLine();
+        int parsed_percentage = int.Parse(percentage);
+
+        if (parsed_percentage >= 90)
+        {
+            Console.WriteLine("You have an A!");
+        }
+
+        else if (parsed_percentage >= 80)
+        {
+            Console.WriteLine("You have a B!");
+        }
+
+        else if (parsed_percentage >= 70)
+        {
+            Console.WriteLine("You have a C.");
+        }
+
+        else if (parsed_percentage >= 60)
+        {
+            Console.WriteLine("You have a D.");
+        }
+
+        else{
+            Console.WriteLine("You have an F...");
+        }
+
+        if (parsed_percentage >= 70)
+        {
+            Console.WriteLine("You passed!!");
+        }
+
+        else 
+        {
+            Console.WriteLine("You did not pass. But you are not defined by your grades. You are more than that. You are a child of God :)");
+        }
     }
 }
---
+++ scripts/demo.csx
@@ -0,0 +1,18 @@
+using System; 
+// using Internal;
+// we have to import everything
+
+Console.WriteLine("Hello, what is your name?!");
+// console is a class, writeLine is a method
+string name = Console.ReadLine();
+// reading what the useer typed into the console, saving it as a string
+// must declare data types
+
+Console.WriteLine($"Hello, {name}!");
+// python's f is c#'s $
+//  CANNOT USE SINGLE QUOTES
+
+
+
+// must be in a class, static void.Main
+// only one Main method in the entire program
