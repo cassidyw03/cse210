7186ef913fdbda92bc24104a92c359170d7145db
cassidyw03 <whi21049@byui.edu>
2024-06-14 15:59:28 -0600

    init

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // Menu menu = new Menu();
+
+        // List<bool> action = new List<bool> {true};
+
+        // foreach (bool action in actions)
+        // {
+        //     if (!menu.ChooseAndRun())
+        //     {
+        //         break;
+        //     }
+        // }
+
+        Console.WriteLine("Welcome to Mindfulness.com! We offer 3 mindfulness activities to unwind with!  :)  Choose below!\n");
+
+        Console.WriteLine(5);
+        Thread.Sleep(1000);
+         Console.WriteLine(4);
+        Thread.Sleep(1000);
+         Console.WriteLine(3);
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Done!");
+
+
+    
+
+        
+
+        // List<string> animations = new List<string>();
+        // animations.Add(".");
+        // animations.Add("..");
+        // animations.Add("...");
+        // animations.Add("");
+        // animations.Add(".");
+        // animations.Add("..");
+        // animations.Add("...");
+        // animations.Add("");
+
+        // foreach (string a in animations)
+        // {
+        //     Console.Write(a);
+        //     Thread.Sleep(1000);
+        //     Console.Write("\b \b");
+        // }
+
+
+        // DateTime startTime = DateTime.Now;
+        // DateTime endTime = startTime.AddSeconds(10);
+
+        // int i = 0;
+
+        // while (DateTime.Now < endTime)
+        // {
+        //     string a = animations[i];
+        //     Console.Write(a);
+        //     Thread.Sleep(1000);
+        //     Console.Write("\b \b");
+
+        //     i++;
+
+        //     if (i >= animations.Count)
+        //     {
+        //         i = 0;
+        //     }
+        // }
+
+        
+
+        // Console.WriteLine("Done!");
     }
 }
